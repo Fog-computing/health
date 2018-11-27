00b7d679fc0873fbb5320d8b02195ca45c8030ee
Fog-computing <tslcm999@163.com>
2018-11-27 16:00:54 +0800

    initial

--- iBase4J-Biz-Facade/src/main/java/cn/ac/sec/util/DateTest.java
+++ iBase4J-Biz-Facade/src/main/java/cn/ac/sec/util/DateTest.java
@@ -4,6 +4,6 @@ import java.util.Date;
 
 public class DateTest {
     public static void main(String args[]){
-        System.out.println (new Date (1512029448000L));
+         System.out.println (new Date (1512029448000L));
     }
 }
